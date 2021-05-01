72582987a98a9b483871aa51527f5446555dc84e
Mariam-Tawfik <74324483+Mariam-Tawfik@users.noreply.github.com>
2021-05-01 12:34:02 +0200

    Add files via upload
    Here are the files for implementing Priority Queue. 
    try to test.

---
+++ CIE205Project/Priority_queue.h
@@ -0,0 +1,18 @@
+#pragma once
+#include"Node.h"
+template < typename T>
+class Priority_queue
+{
+	Node<T> * front;
+	int size;
+public:
+	Priority_queue();
+	bool isEmpty() const;
+	int getSize() const;
+	Node<T>* top();
+	void push(T elemnt);
+	void pop();
+};
+
+
+template class Priority_queue <int>;
